86b27bfe037d29ca2255a5ae294a3078c60c6761
AnushaReddi02 <nushareddi115@gmail.com>
2026-01-02 10:21:26 +0530

    Add index route to display all listings

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
+const path =require('path');
+
+
+
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderly';
 
@@ -16,6 +20,9 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
 //PORT : A number where your server listens
 //request goes to that port
 // process.env.PORT || 3000; -> Works in local + production
@@ -26,18 +33,24 @@ app.get("/", (req,res)=>{
     res.send("Hi I am Home page!");
 });
 
-app.get("/testListing",async (req,res)=>{
-    let sampleListing = new Listing({
-        title : "My home",
-        description : "By the beach",
-        price : 12000,
-        location : "HYD",
-        country : "Ïnd"
-    });
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "My home",
+//         description : "By the beach",
+//         price : 12000,
+//         location : "HYD",
+//         country : "Ïnd"
+//     });
 
-   await sampleListing.save();
-   console.log("Sample was saved");
-   res.send("Sucessful testing");
+//    await sampleListing.save();
+//    console.log("Sample was saved");
+//    res.send("Sucessful testing");
+// })
+
+//Index Route
+app.get("/listings",async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
 })
 // Starts the Express server and listens for incoming requests on the specified port
 // Without this line → your app does NOTHING.

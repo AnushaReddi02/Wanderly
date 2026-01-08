53ce59315ba9fd9ebfa524abe8f410297c4c8917
AnushaReddi02 <nushareddi115@gmail.com>
2026-01-08 20:41:22 +0530

    Add create route for listings

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+/*
+        IMPORTANT ROUTE ORDER NOTE 🚨
+
+        Express matches routes from TOP to BOTTOM.
+
+        If a dynamic route like `/listings/:id` is defined BEFORE
+        a static route like `/listings/new`, then:
+
+        Request:
+            /listings/new
+
+        Will be interpreted as:
+            id = "new"
+
+        Mongoose then tries:
+            Listing.findById("new")
+
+        Which causes this error:
+            CastError: Cast to ObjectId failed for value "new"
+
+        ✅ SOLUTION:
+        Always place STATIC routes (like /new, /edit) 
+        BEFORE dynamic routes (/:id).
+
+        Correct order:
+        1️⃣ /listings/new
+        2️⃣ /listings/:id
+
+*/
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -54,13 +84,29 @@ app.get("/listings",async(req,res)=>{
     res.render("listings/index.ejs",{allListings});
 });
 
+//New Route : Used to create a new listing
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//Create Route : Adds the newly created route to database
+app.post("/listings", async(req,res)=>{
+    // let {title,description,image,price,country,location} = req.body;
+    // new Listing(req.body.listing)
+    // let listing = req.body.listing;
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
 //Show Route : Show entire details of a specific listing(based on ID)
 app.get("/listings/:id", async(req,res)=>{
     let {id} = req.params;
     //Find the listing using id
     const listing = await Listing.findById(id); //It gives entire listing not just id
     res.render("listings/show.ejs",{listing});
-})
+});
+
 
 // Starts the Express server and listens for incoming requests on the specified port
 // Without this line → your app does NOTHING.

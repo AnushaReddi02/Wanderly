907d5acd84144827e06ced71d9dcc94be9207e7d
AnushaReddi02 <nushareddi115@gmail.com>
2026-04-04 10:17:59 +0530

    Improved navigation

--- middlewares.js
+++ middlewares.js
@@ -1,8 +1,16 @@
 module.exports.isLoggedIn = (req,res,next) => {
     console.log(req.user);
      if(!listing){
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error","❌ Listing you requested for doesen't exist");
         return res.redirect("/listings")
     }
     next();
 };
+
+module.exports.saveRedirectUrl = (req,res,next) => {
+    if(req.session.redirectUrl) {
+        res.locals.redirect = req.session.redirectUrl;
+    }
+    next();
+}

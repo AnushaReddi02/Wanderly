e8e6d68b199ce1a6b2875649abcdecef02bbe9e9
AnushaReddi02 <nushareddi115@gmail.com>
2026-04-20 15:43:43 +0530

    Restructured listing route to improve structure and readabilitu using router.route()

--- routes/listing.js
+++ routes/listing.js
@@ -6,28 +6,26 @@ const {isLoggedIn,isOwner,validateListing} = require("../middlewares.js");
 const listingController = require("../controllers/listings.js");
 
 
-
-//Index Route
-router.get("/",wrapAsync(listingController.index));
+router
+   .route("/")
+   .get(wrapAsync(listingController.index))  //Index Route
+   //Create Route : Adds the newly created route to database
+   .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)
+    );
 
 //New Route : Used to create a new listing
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//Show Route : Show entire details of a specific listing(based on ID)
-router.get("/:id", wrapAsync(listingController.showListing));
-
-//Create Route : Adds the newly created route to database
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing)
-);
-
+router
+   .route("/:id")
+   //Show Route : Show entire details of a specific listing(based on ID)
+  .get(wrapAsync(listingController.showListing))
+  //UPDATE ROUTE
+  .put(isLoggedIn,isOwner, wrapAsync(listingController.updateListing))
+  //DELETE Route
+  .delete(isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
 
 //EDIT : Lets us edit an existing listing
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.editListing));
-
-//UPDATE ROUTE
-router.put("/:id", isLoggedIn,isOwner, wrapAsync(listingController.updateListing));
-
-//DELETE Route
-router.delete("/:id", isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
 
 module.exports = router;
